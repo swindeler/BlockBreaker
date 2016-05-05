6b2584b8b4f6c155229bd433797970fabcda7e30
Susi Windeler <susiwindeler@Susannes-MBP.attlocal.net>
2016-05-04 23:48:42 -0700

    Add collider and cause ball to trigger final screen

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	void OnTriggerEnter2D (Collider2D trigger) {
+		Debug.Log ("Trigger");
+		levelManager.LoadLevel ("Win Screen");
+	}
+
+}
